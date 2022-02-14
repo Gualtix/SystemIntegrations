ada60662af54c8d63b985b322fe3bf298a8d6d2b
Gualtix <walkter@gmail.com>
2022-02-14 16:00:51 -0600

    Single File

---
+++ Assigments/Walter/JSoapClient/src/main/java/com/dsu/JSoapClient/JSoapClientApplication.java
@@ -0,0 +1,31 @@
+
+package com.dsu.JSoapClient;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.dsu.JSoapClient.wsdl.GetCountryResponse;
+
+@SpringBootApplication
+public class JSoapClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSoapClientApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner lookup(CountryClient quoteClient) {
+		return args -> {
+		String country = "Spain";
+
+		if (args.length > 0) {
+			country = args[0];
+		}
+		GetCountryResponse response = quoteClient.getCountry(country);
+		System.err.println(response.getCountry().getCurrency());
+		};
+	}
+}
+
