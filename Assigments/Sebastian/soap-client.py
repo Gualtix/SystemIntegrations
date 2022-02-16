6cb6f99261a0d111743e1677f123de75a5d48f82
Sebastian Sanchez <sebas_stt@hotmail.com>
2022-02-15 18:29:00 -0600

    Adding new attributes to python client

--- Assigments/Sebastian/soap-client.py
+++ Assigments/Sebastian/soap-client.py
@@ -1,14 +1,40 @@
-from itertools import product
-from math import prod
-import string
+import datetime
 import zeep
 
-print('Enter the order ID: ')
+status_array = ['STARTED', 'IN_PROGRESS', 'SHIPPED', 'COMPLETED']
+
+print('Enter the order ID: ', end='')
 id = int(input())
 
+print('Enter order date (year): ', end='')
+year = int(input())
+year = 2020 if year < 2000 or year > 2100 else year
+
+print('Enter order date (month): ', end='')
+month = int(input())
+month = 1 if month < 1 or month > 12 else month
+
+print('Enter order date (day): ', end='')
+day = int(input())
+day = 1 if day < 1 or day > 31 else day
+
+date = datetime.datetime(year, month, day)
+
+print('Enter customer name: ', end='')
+name = input()
+
+print("""Enter status
+[0] STARTED
+[1] IN_PROGRESS
+[2] SHIPPED
+[3] COMPLETED :
+""")
+status_index = int(input())
+status_index = 0 if status_index < 0 or status_index > 3 else status_index
+status = status_array[status_index]
 
 client = zeep.Client('http://localhost:8080/orders/ordersservice?wsdl')
-result = client.service.getOrder(id)
+result = client.service.getOrder(id, status, date, name)
 order = zeep.helpers.serialize_object(result)
 
 print('\nOrder ID:', order['orderId'])
